c100cdc679940f8ca181925f24f11bcb6127fcce
rascarlo <rascarlo@gmail.com>
2013-03-04 20:01:44 +0100

    build only mako specific audio files package

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -44,7 +44,7 @@ PRODUCT_LOCALES := en_US
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
 
 # Get some sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+$(call inherit-product, frameworks/base/data/sounds/AudioPackage10.mk)
 
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
